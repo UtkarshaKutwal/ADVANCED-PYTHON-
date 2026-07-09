83f609b2a9f926c6572b789f34142a532e4f6a72
UtkarshaKutwal <khushikutwal5050@gmail.com>
2026-07-09 17:16:51 +0530

    Create greatest num.py

---
+++ greatest num.py
@@ -0,0 +1,16 @@
+# Program to find the greatest among three numbers using nested if
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+if num1 >= num2:
+    if num1 >= num3:
+        print("The greatest number is:", num1)
+    else:
+        print("The greatest number is:", num3)
+else:
+    if num2 >= num3:
+        print("The greatest number is:", num2)
+    else:
+        print("The greatest number is:", num3)
