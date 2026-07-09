beee9a612867b7d5d7d4b66c267ab46551d832f5
UtkarshaKutwal <khushikutwal5050@gmail.com>
2026-07-09 17:22:25 +0530

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,11 @@
+# Program to create and display a dictionary
+
+student = {
+    "Name": "John",
+    "Age": 20,
+    "Course": "Computer Science",
+    "Marks": 85
+}
+
+print("Student Dictionary:")
+print(student)
